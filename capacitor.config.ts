74004ae60ff307aaf8d2d3e729b8c7fe6cb41039
rohit2205046 <157672142+rohit2205046@users.noreply.github.com>
2025-03-20 04:41:44 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,24 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.9ca681eb95ac4c62bc4309273a89bc5e',
-  appName: 'Nebula Jam Stream',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    androidScheme: 'https',
-    allowNavigation: ['*'],
-    cleartext: true
-  },
-  android: {
-    backgroundColor: "#1A1F2C",
-    allowMixedContent: true
-  },
-  ios: {
-    backgroundColor: "#1A1F2C",
-    contentInset: "always",
-    statusBarStyle: "dark"
-  }
+    appId: 'com.example.nebulajamstream',
+    appName: 'Nebula Jam Stream',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+    server: {
+          androidScheme: 'https',
+          allowNavigation: ['*'],
+          cleartext: true
+    },
+    android: {
+          backgroundColor: "#1A1F2C",
+          allowMixedContent: true
+    },
+    ios: {
+          backgroundColor: "#1A1F2C",
+          contentInset: "always",
+          statusBarStyle: "dark"
+    }
 };
 
 export default config;
