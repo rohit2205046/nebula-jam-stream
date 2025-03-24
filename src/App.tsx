48c3a35e50c91df1a12d84f82a5fc3f7bfe24a08
rohit2205046 <157672142+rohit2205046@users.noreply.github.com>
2025-03-24 06:14:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,22 @@ import androidx.fragment.app.FragmentTransaction;
 
 // MainActivity equivalent in Android
 public class MainActivity extends AppCompatActivity {
-          @Override
-          protected void onCreate(Bundle savedInstanceState) {
-                        super.onCreate(savedInstanceState);
-                        setContentView(R.layout.activity_main);
+              @Override
+              protected void onCreate(Bundle savedInstanceState) {
+                                super.onCreate(savedInstanceState);
+                                setContentView(R.layout.activity_main);
 
-              // Initialize fragments
-              FragmentManager fragmentManager = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        Fragment indexFragment = new IndexFragment(); // Assuming you have created these fragments
-              fragmentTransaction.add(R.id.fragment_container, indexFragment);
-                        fragmentTransaction.commit();
-          }
+                  // Initialize fragments
+                  FragmentManager fragmentManager = getSupportFragmentManager();
+                                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                Fragment indexFragment = new IndexFragment(); // Assuming you have created these fragments
+                  fragmentTransaction.add(R.id.fragment_container, indexFragment);
+                                fragmentTransaction.commit();
+              }
 }
 
 // This is a placeholder for your fragment
 public class IndexFragment extends Fragment {
-          // Implement your fragment here
+              // Implement your fragment here
 }
-
 // End of Android conversion code
